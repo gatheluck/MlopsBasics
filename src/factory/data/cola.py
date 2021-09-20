77bf69ff5f6605e85ae73fdebf070b6a9ecada09
gatheluck <gatheluck+tech@gmail.com>
2021-09-20 23:45:46 +0900

    feat: add data module.

---
+++ src/factory/data/cola.py
@@ -0,0 +1,62 @@
+from typing import Final
+
+import pytorch_lightning as pl
+import torch
+from datasets import load_dataset
+
+
+class ColaDataModule(pl.LightningDataModule):
+    def __init__(
+        self,
+        batch_size: int,
+        max_length: int,
+        tokenizer: torch.nn.Module,
+    ):
+        super().__init__()
+
+        self._num_classes: Final = 2
+        self.batch_size: Final = batch_size
+        self.max_length: Final = max_length
+        self.tokenizer: Final = tokenizer
+
+    def prepare_data(self):
+        cola_dataset = load_dataset("glue", "cola")
+        self.train_data = cola_dataset["train"]
+        self.val_data = cola_dataset["validation"]
+
+    def _tokenize_data(self, example):
+        return self.tokenizer(
+            example["sentence"],
+            truncation=True,
+            padding="max_length",
+            max_length=self.max_length,
+        )
+
+    def setup(self, stage=None):
+        # we set up only relevant datasets when stage is specified
+        if stage == "fit" or stage is None:
+            self.train_data = self.train_data.map(self._tokenize_data, batched=True)
+            self.train_data.set_format(
+                type="torch", columns=["input_ids", "attention_mask", "label"]
+            )
+
+            self.val_data = self.val_data.map(self._tokenize_data, batched=True)
+            self.val_data.set_format(
+                type="torch",
+                columns=["input_ids", "attention_mask", "label"],
+                output_all_columns=True,
+            )
+
+    def train_dataloader(self):
+        return torch.utils.data.DataLoader(
+            self.train_data, batch_size=self.batch_size, shuffle=True
+        )
+
+    def val_dataloader(self):
+        return torch.utils.data.DataLoader(
+            self.val_data, batch_size=self.batch_size, shuffle=False
+        )
+
+    @property
+    def num_classes(self) -> int:
+        return self._num_classes
